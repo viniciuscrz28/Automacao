ef0b2b05f437a7c8f9d0735a497352318090d1cd
viniciuscr <168240575+viniciuscrz28@users.noreply.github.com>
2026-06-12 20:28:02 -0300

    a

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,17 +1,3 @@
-function atualizarContador() {
-
-    let carrinho =
-        JSON.parse(localStorage.getItem("carrinho"))
-        || [];
-
-    document.getElementById(
-        "contadorCarrinho"
-    ).textContent = carrinho.length;
-}
-
-atualizarContador();
-
-
 async function carregarComponentes() {
 
     const header =
